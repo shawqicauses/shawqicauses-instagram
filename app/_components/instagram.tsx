2f02ac6e937721e0bcdcc8f79371bc1d17a13cb3
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-28 22:54:30 +0200

    New: Enhance through adding the testimonial slides of the design #01

--- app/_components/instagram.tsx
+++ app/_components/instagram.tsx
@@ -1,15 +1,64 @@
 "use client"
 
-// DONE REVIEWING: GITHUB COMMIT 3️⃣
+// DONE REVIEWING: GITHUB COMMIT 4️⃣
 import DomToImage from "dom-to-image"
 import {ArrowLeftIcon} from "lucide-react"
-import {Fragment} from "react"
+import {Fragment, PropsWithChildren} from "react"
 import {Button} from "../../components/ui"
 
-const InstagramPost = function InstagramPost() {
-  const id = "instagram-design-pattern"
+export const Badge = function Badge({children}: PropsWithChildren) {
+  return (
+    <div className="relative z-30 inline-flex items-center gap-x-6 rounded-full bg-primary/20 px-8 py-4 text-xl-4 font-medium leading-none text-foreground ring-1 ring-inset ring-primary">
+      <svg viewBox="0 0 16 16" aria-hidden="true" className="h-4 w-4 fill-primary">
+        <circle cx={8} cy={8} r={8} />
+      </svg>
+      {children}
+    </div>
+  )
+}
+
+export const Highlight = function Highlight({children}: PropsWithChildren) {
+  return (
+    <span className="relative text-foreground">
+      <div className="absolute left-0 right-0 top-1/2 z-10 h-1/2 w-full -translate-y-1/2 bg-primary" />
+      <span className="relative z-20">{children}</span>
+    </span>
+  )
+}
+
+export const Heading = function Heading({children}: PropsWithChildren) {
+  return (
+    <h1 className="mt-16 text-[6.75rem] font-bold leading-[8.25rem] text-foreground">{children}</h1>
+  )
+}
+
+export const Paragraph = function Paragraph({children}: PropsWithChildren) {
+  return <p className="text-xl-5 font-normal leading-relaxed text-foreground">{children}</p>
+}
+
+export const Footer = function Footer() {
+  return (
+    <div className="mt-auto flex w-full items-center justify-between">
+      <span className="text-[3.375rem] font-bold leading-none">
+        شوقي
+        <span className="text-primary">.</span>
+      </span>
+      <Button variant="primary" size="icon" className="h-20 w-20 !rounded-full" asChild>
+        <span aria-hidden="true">
+          <ArrowLeftIcon strokeWidth={1.5} className="h-10 w-10" />
+        </span>
+      </Button>
+    </div>
+  )
+}
+
+interface PostProps extends PropsWithChildren {
+  id: string
+}
+
+export const Post = function Post({id, children}: PostProps) {
   const download = function download() {
-    DomToImage.toPng(document.getElementById("design-01")).then((value: string) => {
+    DomToImage.toPng(document.getElementById("design-01") as Node).then((value: string) => {
       const anchorElement = document.createElement("a")
       anchorElement.href = value
       anchorElement.download = "design-01.png"
@@ -19,26 +68,30 @@ const InstagramPost = function InstagramPost() {
 
   return (
     <Fragment>
-      <Button onClick={() => download()}>تنزيل الصورة</Button>
-      <hr className="my-10 rounded-full border-border" />
       <div
         id="design-01"
         className="shc-flex-center relative isolate h-[84.375rem] w-[67.5rem] overflow-hidden bg-background">
         <svg
           aria-hidden="true"
-          className="absolute inset-0 h-full w-full stroke-foreground/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]">
+          className="absolute inset-0 h-full w-full stroke-foreground/20 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]">
           <defs>
-            <pattern id={id} width={200} height={200} x="50%" y={-1} patternUnits="userSpaceOnUse">
+            <pattern
+              id={`${id}-pattern`}
+              width={200}
+              height={200}
+              x="50%"
+              y={-1}
+              patternUnits="userSpaceOnUse">
               <path fill="none" d="M.5 200V.5H200" />
             </pattern>
           </defs>
-          <svg x="50%" y={-1} className="overflow-visible fill-border/20">
+          <svg x="50%" y={-1} className="overflow-visible fill-border/25">
             <path
               strokeWidth={0}
               d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
             />
           </svg>
-          <rect width="100%" height="100%" strokeWidth={0} fill={`url(#${id})`} />
+          <rect width="100%" height="100%" strokeWidth={0} fill={`url(#${id}-pattern)`} />
         </svg>
         <div
           aria-hidden="true"
@@ -51,32 +104,8 @@ const InstagramPost = function InstagramPost() {
             }}
           />
         </div>
-        <div className="flex h-full max-h-[50rem] max-w-[55rem] flex-col items-start justify-center">
-          <div className="inline-flex items-center gap-x-6 rounded-full bg-primary/20 px-8 py-4 text-xl-4 font-medium leading-none text-foreground ring-1 ring-inset ring-primary">
-            <svg viewBox="0 0 16 16" aria-hidden="true" className="h-4 w-4 fill-primary">
-              <circle cx={8} cy={8} r={8} />
-            </svg>
-            عاشت غزة
-          </div>
-          <h1 className="relative z-30 mt-16 text-[6.75rem] font-bold leading-[8.25rem] text-foreground">
-            هل تودُّ معرفة <br /> ما الذي يقوله <br />{" "}
-            <span className="relative text-foreground">
-              <div className="absolute bottom-5 left-0 right-0 z-10 h-1/2 w-full bg-primary" />
-              <span className="relative z-20">عملائي</span>
-            </span>{" "}
-            عني 🫡؟
-          </h1>
-          <div className="mt-auto flex w-full items-center justify-between">
-            <span className="text-[3.375rem] font-bold leading-none">
-              شوقي
-              <span className="text-primary">.</span>
-            </span>
-            <Button variant="primary" size="icon" className="h-20 w-20 !rounded-full" asChild>
-              <span aria-hidden="true">
-                <ArrowLeftIcon strokeWidth={1.5} className="h-10 w-10" />
-              </span>
-            </Button>
-          </div>
+        <div className="relative z-30 flex h-full max-h-[50rem] w-full max-w-[55rem] flex-col items-start justify-start">
+          {children}
         </div>
         <div
           aria-hidden="true"
@@ -90,8 +119,7 @@ const InstagramPost = function InstagramPost() {
           />
         </div>
       </div>
+      <Button onClick={() => download()}>تنزيل الصورة</Button>
     </Fragment>
   )
 }
-
-export default InstagramPost
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,44 @@
-// DONE REVIEWING: GITHUB COMMIT 1️⃣
+// DONE REVIEWING: GITHUB COMMIT 2️⃣
 
-import InstagramPost from "./_components/instagram"
+import {Fragment} from "react"
+import {Badge, Footer, Heading, Highlight, Paragraph, Post} from "./_components/instagram"
 
 const Page = async function Page() {
   return (
     <main>
-      <div className="mx-auto max-w-xl-7 py-20">
-        <InstagramPost />
+      <div className="mx-auto max-w-xl-7 space-y-10 py-20">
+        <Post id="design-01">
+          <Badge>عاشت غزة</Badge>
+          <Heading>
+            <Fragment>
+              هل تودُّ معرفة <br /> ما الذي يقوله <br /> <Highlight>عملائي</Highlight> عني 🫡؟
+            </Fragment>
+          </Heading>
+          <Footer />
+        </Post>
+        <Post id="design-01-02">
+          <Paragraph>
+            يود مجلس إدارة شركة عمق للحلول الرقمية في مملكة البحرين أن يتقدم بجزيل الشكر والتقدير
+            للأستاذ{" "}
+            <span className="bg-primary box-decoration-clone text-foreground">شوقي حاتم فارس</span>{" "}
+            على{" "}
+            <span className="bg-primary box-decoration-clone text-foreground">
+              تفانيه في العمل وإجتهاده في طرح الأفكار الإبداعيه
+            </span>{" "}
+            خلال فترة عمله على الموقع الإلكتروني الخاص بالشركة وعليه فقد تقدم بدايةً بطرح فكرةٍ
+            لإعادة تصميم الموقع بحيث يكون تصميم الموقع مواكب لآخر التحديثات الرقمية وبعد موافقة...
+          </Paragraph>
+        </Post>
+        <Post id="design-01-03">
+          <Paragraph>
+            وبعد موافقة القسم المختص على فكرة إعادة التصميم،{" "}
+            <span className="bg-primary box-decoration-clone text-foreground">
+              سُعدنا بسرعة إنجاز العمل بأسرع وقت ممكن وهذا ما كنا نطمح له
+            </span>
+            ، وكما أننا ننصح الشركات والمؤسسات بالتعامل معه، فنحن نتطلع للتعامل معه في المستقبل
+            القريب، متمنين لك دوام التوفيق والنجاح.
+          </Paragraph>
+        </Post>
       </div>
     </main>
   )
