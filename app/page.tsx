6a3ff624a09b1c55572c49a726fe9adfddfda285
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-29 17:27:40 +0200

    New: Enhance through adding stars in the design #01

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-// DONE REVIEWING: GITHUB COMMIT 2️⃣
+// DONE REVIEWING: GITHUB COMMIT 3️⃣
 
+import {StarIcon} from "lucide-react"
 import {Fragment} from "react"
 import {Badge, Footer, Heading, Highlight, Paragraph, Post} from "./_components/instagram"
 
@@ -17,6 +18,11 @@ const Page = async function Page() {
           <Footer />
         </Post>
         <Post id="design-01-02">
+          <div className="-mb-10 flex gap-x-1.5 text-primary">
+            {[0, 1, 2, 3, 4].map((element) => (
+              <StarIcon key={element} aria-hidden="true" className="h-8 w-8 fill-current" />
+            ))}
+          </div>
           <Paragraph>
             يود مجلس إدارة شركة عمق للحلول الرقمية في مملكة البحرين أن يتقدم بجزيل الشكر والتقدير
             للأستاذ{" "}
@@ -30,6 +36,11 @@ const Page = async function Page() {
           </Paragraph>
         </Post>
         <Post id="design-01-03">
+          <div className="-mb-10 flex gap-x-1.5 text-primary">
+            {[0, 1, 2, 3, 4].map((element) => (
+              <StarIcon key={element} aria-hidden="true" className="h-8 w-8 fill-current" />
+            ))}
+          </div>
           <Paragraph>
             وبعد موافقة القسم المختص على فكرة إعادة التصميم،{" "}
             <span className="bg-primary box-decoration-clone text-foreground">
